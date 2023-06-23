410f034314a6289207d890d3dc4439a89c8336ea
dushyantsapra <dushyant.sapra@gmail.com>
2023-06-23 13:41:45 +0530

    Addded JWT token generation and validation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dushyantsapra/PracticeGo
 
 go 1.18
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- jwt/main.go
+++ jwt/main.go
@@ -8,12 +8,13 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-func SayHello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Caller"))
-}
-
-var SECRET = []byte("super-secret-auth-key")
+var SECRET = []byte("secret-auth-key")
 var api_key = "1234"
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("Hello Caller from secure Area"))
+	fmt.Fprintf(w, "Hello Caller from secure Area")
+}
 
 func CreateJWT() (string, error) {
 
@@ -22,6 +23,7 @@ func CreateJWT() (string, error) {
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["exp"] = time.Now().Add(time.Hour).Unix()
+	claims["username"] = "Dushyant"
 
 	tokenStr, err := token.SignedString(SECRET)
 
@@ -33,8 +35,50 @@ func CreateJWT() (string, error) {
 	return tokenStr, nil
 }
 
+func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(t *jwt.Token) (interface{}, error) {
+				_, ok := t.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("not authorized"))
+				}
+				return SECRET, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("not authorized\n"))
+			}
+
+			if token.Valid {
+				next(w, r)
+			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("not authorized\n"))
+		}
+	})
+}
+
+func GetJWT(w http.ResponseWriter, r *http.Request) {
+	if r.Header["Access"] != nil {
+		if r.Header["Access"][0] != api_key {
+			return
+		} else {
+			jwtStr, err := CreateJWT()
+			if err != nil {
+				return
+			}
+			fmt.Fprint(w, jwtStr)
+		}
+	}
+}
+
 func main() {
-	http.HandleFunc("/hello", SayHello)
+	http.Handle("/hello", ValidateJWT(SayHello))
+	http.HandleFunc("/getJwt", GetJWT)
 
 	http.ListenAndServe(":8080", nil)
 }
